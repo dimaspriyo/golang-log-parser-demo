0f8dda4b74c33e234f6929048eeadc936c7e8d8a
dimaspriyo <dimspriyo@gmail.com>
2021-04-30 12:18:33 +0700

    init

---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+
+	_ "github.com/davecgh/go-spew/spew"
+)
+
+type Log struct {
+	IP          string `json:"ip"`
+	Timestamp   string `json:"timestamp"`
+	HttpMethod  string `json:"http_method"`
+	Parameter   string `json:"parameter"`
+	HttpVersion string `json:"http_version"`
+	Status      string `json:"status"`
+	Byte        string `json:"byte"`
+	URL         string `json:"url"`
+	UserAgent   string `json:"user_agent"`
+}
+
+func main() {
+
+	// formatFlag := flag.String("t", "PlainText", "Defined Format")
+	// outputFlag := flag.String("o", "no-opt", "Output Log")
+	// fileArg := os.Args[1]
+
+	// flag.Parse()
+	// fmt.Println(*formatFlag)
+	// fmt.Println(*outputFlag)
+
+	res, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		panic(err.Error())
+	}
+
+	regPattern := regexp.MustCompile(`(?P<ip>.+)-(.+)-(.+)\[(?P<timestamp>.+)]\s\"(?P<http_method>.+?)\s(?P<parameter>.+?)\s(?P<http_version>.+?)\"\s(?P<status>.+?)\s(?P<byte>.+?)\s"(?P<url>.+?)"\s"(?P<user_agent>.+?)"`)
+
+	temp := make(map[string]interface{})
+	match := regPattern.FindStringSubmatch(string(res))
+	for k, v := range regPattern.SubexpNames() {
+		if v == "" {
+			continue
+		}
+		temp[v] = match[k]
+	}
+
+	jsonRes, err := json.Marshal(temp)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	log := Log{}
+	err = json.Unmarshal(jsonRes, &log)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	listArgs := os.Args[2:]
+
+	formatFlag := "PlainText"
+	outputFlag := "no"
+
+	formatPosition := 0
+	outputPosition := 0
+
+	for k, v := range listArgs {
+		if v == "-t" {
+			formatPosition = k + 1
+			formatFlag = listArgs[formatPosition]
+		}
+
+		if v == "-o" {
+			outputPosition = k + 1
+			outputFlag = listArgs[outputPosition]
+		}
+	}
+
+	response := ""
+
+	if formatFlag == "json" {
+		response = string(jsonRes)
+	} else {
+		response = string(res)
+	}
+
+	if outputFlag == "no" {
+		fmt.Println(response)
+	} else {
+		fmt.Println("yes output")
+		err := ioutil.WriteFile(outputFlag, []byte(response), 0644)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+}
